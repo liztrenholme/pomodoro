2b3d93c2b40af3f34dfb44e469ad48b8855a68c3
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-08-30 21:16:43 -0400

    hook for timeout

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './display.css';
 import PropTypes from 'prop-types';
 
-class Display extends Component {
-  render() {
+const Display = (props) => {
+  useEffect((time) => {
+    const timer = setTimeout(() => {
+      console.log('This will run after 1 second!')
+      alert(time)
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
     return (
       <div className='display'>
         <div style={{
           width: '20em', 
           height: '10em', 
-          backgroundColor: this.props.color, 
+          backgroundColor: props.color, 
           border: '5px solid #fff',
           borderRadius: '15px'
         }}
         />
       </div>
     );
-  }
 }
 
 Display.propTypes = {
-  color: PropTypes.string
+  color: PropTypes.string,
+  time: PropTypes.number
 };
 
 export default Display;
